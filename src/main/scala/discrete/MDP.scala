4bdbe2fedd4e6278825cd278932c35b54327bc03
Eric <eric@Larkins-MacBook-Air.local>
2018-12-14 00:33:36 -0500

    added directional algorithm for gride example

--- src/main/scala/discrete/MDP.scala
+++ src/main/scala/discrete/MDP.scala
@@ -41,29 +41,72 @@ object MDP {
         this.discountFactor = dFactor
     }
 
-    def getRewardFromGrid(row: Int, col: Int): Array[Tuple2[Double, Double]] = {
+    def getAdjIndices(row: Int, col: Int): Array[Tuple2[Int, Int]] = {
         
-        if(valueGrid.length < 1) return(Array[Tuple2[Double, Double]]())
+        if(rewardGrid.length < 1) return(Array[Tuple2[Int, Int]]())
 
-        val colX = valueGrid(0).length - 1
-        val rowY = valueGrid.length - 1
-        val tup = (row, col)
-        println(tup)
+        val colX = rewardGrid(0).length - 1
+        val rowY = rewardGrid.length - 1
 
-        tup match {
-            case (0, 0) => Array((0,1), (1,0))
-            case (0, colX)  => Array((0, colX - 1), (1, colX))
-            case (rowY, 0)  => Array((rowY - 1, 0), (rowY, 1))
-            case (rowY, colX) => Array((rowY - 1, colX), (rowY, colX - 1))
-            case (0, _) => Array((0, col + 1), (0, col -1 ), (1, col))
-            case (rowY, _) => Array( (row - 1, col), (row, col -1 ), (row, col + 1))
-            case (_, colX) => Array( (row - 1, col), (row + 1, col ), (row, col - 1))
+        (row, col) match {
+            case (0, 0) => println("case 1"); Array((0,1), (1,0))
+            case (0, `colX`)  => println("case 2"); Array((0, colX - 1), (1, colX))
+            case (0, _) => println("case 5"); Array((0, col + 1), (0, col -1 ), (1, col))
+            case (`rowY`, 0)  => println("case 3"); Array((rowY - 1, 0), (rowY, 1))
+            case (`rowY`, `colX`) => println("case 4"); Array((rowY - 1, colX), (rowY, colX - 1))
+            case (`rowY`, _) => Array( (row - 1, col), (row, col -1 ), (row, col + 1))
+            case (_, `colX`) => Array( (row - 1, col), (row + 1, col ), (row, col - 1))
             case (_, 0) => Array((row, 1), (row - 1, 0), (row + 1, 0) )
-            case _ => Array((row -1, col), (row + 1, col), (row, col - 1), (row, col - 1))
+            case _ => Array((row -1, col), (row + 1, col), (row, col - 1), (row, col + 1))
+        }    
+    }
+
+    def getActionIndices(dir: String, row: Int, col: Int): Array[Tuple2[Int, Int]] = {
+        val adjInd = getAdjIndices(row, col)
+        var dirInd = Array[Tuple2[Int, Int]]()
+        if (dir == "N"){
+            if (adjInd contains (row - 1, col)) dirInd = dirInd ++ Array((row - 1, col))
+            if (adjInd contains (row, col + 1)) dirInd = dirInd ++ Array((row, col + 1))
+            if (adjInd contains (row, col - 1)) dirInd = dirInd ++ Array((row, col - 1))
+        } else if (dir == "S"){
+            if (adjInd contains (row + 1, col)) dirInd = dirInd ++ Array((row + 1, col))
+            if (adjInd contains (row, col + 1)) dirInd = dirInd ++ Array((row, col + 1))
+            if (adjInd contains (row, col - 1)) dirInd = dirInd ++ Array((row, col - 1))
+        } else if (dir == "E") {
+            if (adjInd contains (row + 1, col)) dirInd = dirInd ++ Array((row + 1, col))
+            if (adjInd contains (row - 1, col)) dirInd = dirInd ++ Array((row - 1, col))
+            if (adjInd contains (row, col + 1)) dirInd = dirInd ++ Array((row, col + 1))
+        } else if (dir == "W") {
+            if (adjInd contains (row + 1, col)) dirInd = dirInd ++ Array((row + 1, col))
+            if (adjInd contains (row - 1, col)) dirInd = dirInd ++ Array((row - 1, col))
+            if (adjInd contains (row, col - 1)) dirInd = dirInd ++ Array((row, col - 1))
+        }
+        dirInd
+    }
+
+
+
+    def valueIterate(row: Int, col: Int): Unit = {
+        val indices = getAdjIndices(row, col)
+        for (t <- indices){
+            valueGrid(t._1)(t._2) = rewardGrid(t._1)(t._2) + discountFactor * valueGrid(t._1)(t._2)
         }
     }
-        
+
+    def getRewardFromGrid(row: Int, col: Int): Array[Double] = {
+        val indices = getAdjIndices(row, col)
+        indices.map(t => rewardGrid(t._1)(t._2))
     }
+
+    def getValueFromGrid(row: Int, col: Int): Array[Double] = {
+        val indices = getAdjIndicies(row, col)
+        indices.map(t => valueGrid(t._1)(t._2))
+    }
+
+    
+    
+
+
         // Corner point condition
         // Use conention left right up down
         
