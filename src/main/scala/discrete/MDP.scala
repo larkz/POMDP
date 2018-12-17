1b5422102261cba86ce8af7a29e5dc5e01238cdd
Eric <eric@Larkins-MacBook-Air.local>
2018-12-16 21:43:02 -0500

    working mdp

--- src/main/scala/discrete/MDP.scala
+++ src/main/scala/discrete/MDP.scala
@@ -20,7 +20,7 @@ object MDP {
     var valueGrid = Array[Array[Double]]() // iterative
     var optimalPolicyGrid = Array[Array[Double]]() //iterative
     var rewardGrid = Array[Array[Double]]() //fixed
-    var discountFactor = 1.0 //fixed
+    var discountFactor = 0.8 //fixed
     var transitionProbabilities = Array[Array[Double]]() //fixed, assume fixed through out
 
     def getValueGrid(): Array[Array[Double]] = {
@@ -33,8 +33,8 @@ object MDP {
 
     def setRewardGrid(rGrid: Array[Array[Double]]): Unit = {
         this.rewardGrid = rGrid
-        this.valueGrid = Array.ofDim[Int](rGrid(0).length, rGrid.length)
-        this.optimalPolicyGrid = Array.ofDim[Int](rGrid(0).length, rGrid.length)
+        this.valueGrid = Array.ofDim[Double](rGrid(0).length, rGrid.length)
+        this.optimalPolicyGrid = Array.ofDim[Double](rGrid(0).length, rGrid.length)
     }
 
     def setDiscountFactor(dFactor: Double): Unit = {
@@ -85,20 +85,20 @@ object MDP {
         dirInd
     }
 
-    def valueIterateAtIndex(row: Int, col: Int): Unit = {
-        val actions = Array("N", "S", "E", "W")
-        for(a <- actions){
-            valueIterateAtIndexWithAction(a, row, col)
-        }
-    }
-
     def valueIterateAtIndexWithAction(dir: String, row: Int, col: Int): Unit = {
         val indices = getActionIndices(dir, row, col)
         for (i <- indices){
             val p = i._1
             val t = i._2
             val valueAtIndex = p * (rewardGrid(t._1)(t._2) + discountFactor * valueGrid(t._1)(t._2))
-            valueGrid(t._1)(t._2) = valueGrid(t._1)(t._2) + valueAtIndex
+            valueGrid(t._1)(t._2) = valueGrid(row)(col) + valueAtIndex
+        }
+    }
+
+    def valueIterateAtIndex(row: Int, col: Int): Unit = {
+        val actions = Array("N", "S", "E", "W")
+        for(a <- actions){
+            valueIterateAtIndexWithAction(a, row, col)
         }
     }
 
@@ -109,38 +109,12 @@ object MDP {
                     valueIterateAtIndex(r, c)
                 }
             }
-        print(valueGrid)
+        print(valueGrid.deep)
+        print("\n")
         }
     }
-        
     
-    def getRewardFromGrid(row: Int, col: Int): Array[Double] = {
-        val indices = getAdjIndices(row, col)
-        indices.map(t => rewardGrid(t._1)(t._2))
-    }
-
-    def getValueFromGrid(row: Int, col: Int): Array[Double] = {
-        val indices = getAdjIndicies(row, col)
-        indices.map(t => valueGrid(t._1)(t._2))
-    }
-
-    
-    
-
-
-        // Corner point condition
-        // Use conention left right up down
-        
-
-
+    // Corner point condition
+    // Use conention left right up down
     // Grid world initialization
-    def valueOutput(x: Int, y: Int, H: Int): Unit = {
-        
-    }
-
-    def writeToCSV(df: DataFrame, fileName: DataFrame): Unit = {
-        val spark = SparkSession.builder().getOrCreate()
-        println("writing dataframe to csv!")
-        df.write.format("com.databricks.spark.csv").option("header", "true").save("mydata.csv")
-    }
 }
